c938b41d166fefb428c70dcc19481ab209efc9e7
fandi-ahmad <fandi4160@gmail.com>
2023-08-01 14:25:37 +0800

    add sweetalert in base

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -21,6 +21,7 @@
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0">@yield('title')</h1>
+            @include('sweetalert::alert')
           </div>
         </div>
       </div>
