4d16d8aa659ead95391a3a1c338e777d915c35fe
Suryawandivta <gitdivta@gmail.com>
2023-08-01 10:26:55 +0800

    feat(PRV-21/setup): Membuat Crud api table setup

---
+++ app/Http/Controllers/API/SetupController.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Ramsey\Uuid\Uuid;
+use App\Models\SetupModel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+
+class SetupController extends Controller
+{
+    public function getAllData()
+    {
+        $data = SetupModel::all();
+        if($data->isEmpty()){
+            return response()->json([
+                'code' => 404,
+                'message' => 'Data not found'
+            ]);
+        }else{
+            return response()->json([
+                'code' => 200,
+                'message' => 'Get all data successfully',
+                'data' => $data
+            ]);
+        }
+    }
+
+    public function createData(Request $request)
+    {
+        $validation = Validator::make(
+            $request->all(),
+            [
+                'title' => 'required',
+                'deskripsi'=>'required',
+                'gambar'=> 'mimes:png,jpg,jpeg',
+            ],
+            [
+                'title.required'=>'Form title tidak boleh kosong',
+                'deskripsi.required'=>'Form deskripsi tidak boleh kosong',
+                'gambar.mimes'=>'Gambar harus dalam format PNG, JPG, atau JPEG '
+            ]
+        );
+
+        if($validation->fails()){
+            return response()->json([
+                'code' =>422,
+                'message' =>'Check your validation',
+                'errors' =>$validation->errors()
+            ]);
+        }
+
+        try {
+            $data = new SetupModel();
+            $data->uuid = Uuid::uuid4()->toString();
+            $data->title = $request->input('title');
+            $data->deskripsi = $request->input('deskripsi');
+            if ($request->hasFile('gambar')) {
+                $file = $request->file('gambar');
+                $extention = $file->getClientOriginalExtension();
+                $filename = 'SETUP-' . Str::random(15) . '.' . $extention;
+                Storage::makeDirectory('uploads/setup/');
+                $file->move(public_path('uploads/setup/'), $filename);
+                $data->gambar = $filename;
+            }
+            $data->save();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Failed to create data',
+                'errors' => $th->getMessage(),
+            ]);
+        }
+
+        return response()->json([
+            'code' => 200,
+            'message' => 'Create data successfully',
+            'data' => $data
+        ]);
+    }
+
+    public function getDataByUuid($uuid)
+    {
+        if (!Uuid::isValid($uuid)) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Uuid is failed',
+            ]);
+        }
+
+        $data = SetupModel::where('uuid', $uuid)->first();
+        if (!$data) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Data not found',
+            ]);
+        } else {
+            return response()->json([
+                'code' => 200,
+                'message' => 'Get data by UUID successfully',
+                'data' => $data
+            ]);
+        }
+    }
+
+    public function updateDataByUuid(Request $request, $uuid)
+    {
+        $validation = Validator::make(
+            $request->all(),
+            [
+                'title' => 'required',
+                'deskripsi' => 'required',
+                'gambar' => 'mimes:png,jpg,jpeg',
+            ],
+            [
+                'title.required' => 'Form title tidak boleh kosong',
+                'deskripsi.required' => 'Form deskripsi tidak boleh kosong',
+                'gambar.mimes' => 'Gambar harus dalam format PNG, JPG, atau JPEG '
+            ]
+        );
+
+        if ($validation->fails()) {
+            return response()->json([
+                'code' => 422,
+                'message' => 'Check your validation',
+                'errors' => $validation->errors()
+            ]);
+        }
+        try {
+            $data = SetupModel::where('uuid', $uuid)->firstOrFail();
+            $data->title = $request->input('title');
+            $data->deskripsi = $request->input('deskripsi');
+            if ($request->hasFile('gambar')) {
+                $file = $request->file('gambar');
+                $extention = $file->getClientOriginalExtension();
+                $filename = 'SETUP-' . Str::random(15) . '.' . $extention;
+                Storage::makeDirectory('uploads/setup/');
+                $file->move(public_path('uploads/setup/'), $filename);
+                $old_file_path = public_path('uploads/setup/') . $data->gambar;
+                if (file_exists($old_file_path)) {
+                    unlink($old_file_path);
+                }
+                $data->gambar = $filename;
+            }
+            $data->save();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Failed to update data',
+                'errors' => $th->getMessage(),
+            ]);
+        }
+
+        return response()->json([
+            'code' => 200,
+            'message' => 'Updated data successfully',
+            'data' => $data
+        ]);
+    }
+
+    public function deleteData($uuid)
+    {
+        if (!Uuid::isValid($uuid)) {
+            return response()->json([
+                'code' => 404,
+                'message' => 'Data Not Found',
+            ]);
+        }
+
+        try {
+            $data = SetupModel::where('uuid', $uuid)->first();
+            if (!$data) {
+                return response()->json([
+                    'code' => 404,
+                    'message' => 'Data Not Found',
+                ]);
+            }
+
+            $filePath = 'uploads/setup/' . $data->gambar;
+            if (File::exists($filePath)) {
+                File::delete($filePath);
+            }
+            $data->delete();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Failed to delete data',
+                'errors' => $th->getMessage()
+            ]);
+        }
+
+        return response()->json([
+            'code' => 200,
+            'message' =>'Delete data successfuly'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\PendaftaranController;
+use App\Http\Controllers\Api\SetupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,4 +28,12 @@ Route::prefix('v1')->controller(PendaftaranController::class)->group(function ()
     Route::put('/4a3f479a-eb2e-498f-aa7b-e7d6e3f0c5f3/pendaftaran/update/{uuid}', 'updateData');
     Route::delete('/83df59b0-7c1a-4944-8fbb-2c06670dfa01/pendaftaran/delete/{uuid}', 'deleteData');
     Route::get('/febba411-89e8-4fb3-9f55-85c56dcff41d/wa/{email}', 'toLinksWa');
+});
+//API setup
+Route::prefix('v2')->controller(SetupController::class)->group(function () {
+    Route::get('/dd0af7cb-a745-4810-a12c-cefa8a4b24d8/setup', 'getAllData');
+    Route::post('/dd0af7cb-a745-4810-a12c-cefa8a4b24d8/setup/create', 'createData');
+    Route::get('/dd0af7cb-a745-4810-a12c-cefa8a4b24d8/setup/get/{uuid}', 'getDataByUuid');
+    Route::post('/dd0af7cb-a745-4810-a12c-cefa8a4b24d8/setup/update/{uuid}', 'updateDataByUuid');
+    Route::delete('/dd0af7cb-a745-4810-a12c-cefa8a4b24d8/setup/delete/{uuid}', 'deleteData');
 });
