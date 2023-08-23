9324f608bbec413a96a3fb1cf10a57e2c85615ac
Suryawandivta <gitdivta@gmail.com>
2023-08-23 22:37:13 +0800

    update ui news

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -10,43 +10,43 @@ let prevTranslate = 0;
 slider.style.transform = `translateX(${currentTranslate}px)`;
 
 function touchmove(e) {
-  if (!isDragging) return;
-
-  const currentPosition = e.touches[0].clientX;
-  const distance = currentPosition - startPos;
-  currentTranslate = prevTranslate + distance;
-
-  slider.style.transform = `translateX(${currentTranslate}px)`;
-}
-
-if (cardCount > 1) {
-  slider.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    startPos = e.clientX;
-    slider.style.cursor = "grabbing";
-
-    document.addEventListener("mousemove", mousemove);
-    document.addEventListener("mouseup", mouseup);
-  });
-
-  function mousemove(e) {
     if (!isDragging) return;
 
-    const currentPosition = e.clientX;
+    const currentPosition = e.touches[0].clientX;
     const distance = currentPosition - startPos;
     currentTranslate = prevTranslate + distance;
 
     slider.style.transform = `translateX(${currentTranslate}px)`;
-  }
+}
 
-  function mouseup() {
-    document.removeEventListener("mousemove", mousemove);
-    document.removeEventListener("mouseup", mouseup);
+if (cardCount > 1) {
+    slider.addEventListener("mousedown", (e) => {
+        isDragging = true;
+        startPos = e.clientX;
+        slider.style.cursor = "grabbing";
 
-    isDragging = false;
-    prevTranslate = currentTranslate;
+        document.addEventListener("mousemove", mousemove);
+        document.addEventListener("mouseup", mouseup);
+    });
 
-    slider.style.cursor = "grab";
-  }
+    function mousemove(e) {
+        if (!isDragging) return;
+
+        const currentPosition = e.clientX;
+        const distance = currentPosition - startPos;
+        currentTranslate = prevTranslate + distance;
+
+        slider.style.transform = `translateX(${currentTranslate}px)`;
+    }
+
+    function mouseup() {
+        document.removeEventListener("mousemove", mousemove);
+        document.removeEventListener("mouseup", mouseup);
+
+        isDragging = false;
+        prevTranslate = currentTranslate;
+
+        slider.style.cursor = "grab";
+    }
 }
 
