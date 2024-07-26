665449531083144d12444c26510c68fe6761b091
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-27 00:56:22 +0800

    feat(admin):handle API in admin

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -5,6 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>PENA Profile</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <script>
+   let appUrl = '{{ env('APP_URL2') }}';
+  </script>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
