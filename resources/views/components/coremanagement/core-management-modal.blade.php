ef6dde15b4632b09a86305e919acbdee5a314845
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-27 20:09:58 +0800

    feat(coremanagement):finish all featur

--- resources/views/components/coremanagement/core-management-modal.blade.php
+++ resources/views/components/coremanagement/core-management-modal.blade.php
@@ -4,31 +4,27 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="coreManagementLabel">Tambah Data</h5>
+                    <h5 class="modal-title" id="modal-title">Tambah Data</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    <form id="formTambah">
+                <form id="formTambah">
+                    <div class="modal-body">
                         @csrf
                         <div class="row py-2">
-                            <div class="col-md-12">
-                                <div class="text-center">
-                                    <img src="" alt="" id="preview" class="mx-auto d-block pb-2"
-                                        style="max-width: 200px; padding-top: 23px">
-                                </div>
+                            <div class="col-md-12" id="form-preview">
                             </div>
                             <div class="col-md-12">
-                                <div class="form-group fill">
-                                    <input type="hidden" name="uuid" id="uuid" value="">
+                                <div class="form-group fill form-show-validation">
+                                    <input type="hidden" name="id" id="id" value="">
                                     <label>Nama</label>
-                                    <input id="nama" name="nama" type="text" class="form-control"
-                                        placeholder="input here..." autocomplete="off">
+                                    <input id="name" name="name" type="text" class="form-control"
+                                        placeholder="Nama anda" autocomplete="off">
                                 </div>
-                                <div class="form-group fill">
+                                <div class="form-group fill form-show-validation">
                                     <label>Jabatan</label>
-                                    <select name="jabatan" id="jabatan" class="form-control" >
+                                    <select name="jabatan" id="jabatan" class="form-control">
                                         <option value="" selected disabled hidden>Choose here</option>
                                         <option value="ketua_umum">Ketua Umum</option>
                                         <option value="pembina">Pembina</option>
@@ -37,22 +33,19 @@
                                         <option value="waketum">Wakil Ketua Umum</option>
                                     </select>
                                 </div>
-                                <div class="form-group">
-                                    <label for="gambar">Gambar</label>
-                                    <div class="custom-file">
-                                        <input type="file" class="custom-file-input" id="gambar" name="gambar">
-                                        <label class="custom-file-label" for="gambar" id="gambar-label">Upload
-                                            gambar</label>
-                                    </div>
+                                <div class="form-group form-show-validation">
+                                    <label for="photo">Gambar</label>
+                                    <input type="file" class="form-control " style="height: 44px !important" name="photo" id="photo">
                                 </div>
                             </div>
                         </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-outline-primary">Submit Data</button>
-                </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-outline-primary">Simpan Data</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
