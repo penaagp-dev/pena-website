55478a12c2552dd44bccf1d2bd296428814ef380
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-27 20:10:03 +0800

    feat(coremanagement):finish all featur

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -33,6 +33,7 @@
   <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('global.css') }}">
 
   <!-- jQuery -->
 <script src="{{ asset('assets/plugins/jquery/jquery.min.js')}}"></script>
