1845264f44cea350ca6ad710595d9055ca33a271
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-29 09:10:42 +0800

    feat(registerca):create featur register ca

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Interfaces\CoreManagementInterfaces;
+use App\Interfaces\RegisterCaInterfaces;
+use App\Repositories\CoreManagementRepositories;
+use App\Repositories\RegisterCaRepositories;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CoreManagementInterfaces::class, CoreManagementRepositories::class);
+        $this->app->bind(RegisterCaInterfaces::class, RegisterCaRepositories::class);
     }
 
     /**
