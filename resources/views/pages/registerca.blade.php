36a49d6e93cf9d1453a7d1ca5b0da569776d7e40
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-29 09:12:25 +0800

    feat(registerca):create featur register ca

---
+++ resources/views/pages/registerca.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.base')
+{{-- @section('title')
+    Gallery
+@endsection --}}
+@section('content')
+    <div class="card">
+        <x-base-header headerTitle="Data calon anggota" buttonAdd="true" headerAddButton="Tambah Data"
+            modalId="#registerCaModal"></x-base-header>
+        <x-base-body>
+            <x-base-table initId="dataTable">
+                <x-slot name="thead">
+                    <tr>
+                        <th>Info umum</th>
+                        <th>Info kampus</th>
+                        <th>Alasan bergabung</th>
+                        <th>Agama</th>
+                        <th>Gambar</th>
+                        <th>Status</th>
+                        <th>Aksi</th>
+                    </tr>
+                </x-slot>
+            </x-base-table>
+        </x-base-body>
+    </div>
+
+    <x-registerca.registerca-modal></x-registerca.registerca-modal>
+    <script type="module" src="{{ asset('js/registerca/registerca.controller.js') }}"></script>
+@endsection
