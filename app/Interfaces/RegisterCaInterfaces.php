835f10cb369bf5df3cb5cc1f9c50b6293a7833e5
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-29 09:10:33 +0800

    feat(registerca):create featur register ca

---
+++ app/Interfaces/RegisterCaInterfaces.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Http\Requests\RegisterCa\RegisterCaRequest;
+
+interface RegisterCaInterfaces
+{
+    public function getAllData();
+    public function createData(RegisterCaRequest $request);
+    public function getDataById($id);
+    public function updateData(RegisterCaRequest $request, $id);
+    public function deleteData($id);
+}
