cccab0296c9f0a0a611e5a283fe2fac559df9ebd
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-29 11:13:21 +0800

    feat(registerca):create API register ca for fe

--- app/Repositories/RegisterCaRepositories.php
+++ app/Repositories/RegisterCaRepositories.php
@@ -3,11 +3,16 @@
 
 namespace App\Repositories;
 
+use App\Helper\EmailHandler;
 use App\Helper\ImageHandler;
 use App\Http\Requests\RegisterCa\RegisterCaRequest;
 use App\Interfaces\RegisterCaInterfaces;
+use App\Mail\registerCaMail;
 use App\Models\RegisterCaModel;
 use App\Traits\HttpResponseTrait;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class RegisterCaRepositories implements RegisterCaInterfaces
 {
@@ -108,6 +113,54 @@ class RegisterCaRepositories implements RegisterCaInterfaces
             }
             $data->delete();
             return $this->delete();
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 500);
+        }
+    }
+
+    public function registerCaFe(RegisterCaRequest $request)
+    {
+        try {
+            $data = new $this->registerCaModel;
+            $data->name = $request->input('name');
+            $data->date_of_birth = $request->input('date_of_birth');
+            $data->religion = $request->input('religion');
+            $data->email = $request->input('email');
+            $data->phone = $request->input('phone');
+            $data->major = $request->input('major');
+            $data->semester = $request->input('semester');
+            $data->gender = $request->input('gender');
+            $data->address = $request->input('address');
+            $data->reason_register = $request->input('reason_register');
+            $data->status = 'pending';
+            if ($request->hasFile('photo')) {
+                $data->photo = ImageHandler::uploadImage($request->file('photo'), 'uploads/registerca', 'REGISTER-CA-');
+            }
+            $data->email_token = Str::random(32);
+            $data->expires_at = Carbon::now()->addMinutes(1);
+            
+            $data->save();
+            EmailHandler::sendWaLink($data->email, $data);
+
+            return $this->success($data, 'success', 'success create data register ca');
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 500);
+        }
+    }
+
+    public function verifyEmailExp($token)
+    {
+        try {
+            $data = $this->registerCaModel->where('email_token', $token)->first();
+            if(!$data){
+                return view('emails.expiredEmail');
+            }
+
+            if (!$data->expires_at || Carbon::now()->gt($data->expires_at)) {
+                return view('emails.expiredEmail');
+            }
+
+            return redirect('https://google.com');
         } catch (\Throwable $th) {
             return $this->error($th->getMessage(), 500);
         }
