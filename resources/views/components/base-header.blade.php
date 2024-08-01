59b6a3a85143c24ac2534c038f544beee9aae22d
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-01 21:26:35 +0800

    feat(export):create export featur

--- resources/views/components/base-header.blade.php
+++ resources/views/components/base-header.blade.php
@@ -1,10 +1,16 @@
-@props(['headerTitle', 'headerAddButton', 'buttonAdd', 'modalId'])
+@props(['headerTitle', 'headerAddButton', 'buttonAdd', 'modalId', 'buttonExport', 'exportId'])
 
 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
     <h6 class="m-0 font-weight-bold">{{ $headerTitle }}</h6>
-    @if ($buttonAdd == 'true')
-        <button type="button" class="btn btn-outline-primary ml-auto" data-toggle="modal" data-target="{{ $modalId }}">
-            <i class="fas fa fa-plus"></i> {{ $headerAddButton }}
-        </button>
-    @endif
+    <div class="ml-auto">
+        @if ($buttonExport == 'true')
+            <i class="fas fa-file-excel fa-2xl pr-3 text-success" style="cursor: pointer;" id="{{ $exportId }}"></i>
+        @endif
+        @if ($buttonAdd == 'true')
+            <button type="button" class="btn btn-outline-primary ml-auto" data-toggle="modal"
+                data-target="{{ $modalId }}">
+                <i class="fas fa fa-plus"></i> {{ $headerAddButton }}
+            </button>
+        @endif
+    </div>
 </div>
