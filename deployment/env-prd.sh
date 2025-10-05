8bce849170bcfe467643321aa2b51ba9495482fe
sofyan48 <meongbego@gmail.com>
2025-10-05 23:01:11 +0700

    Fix vault path in env-prd.sh to correctly reference pena/prd-pena-web

--- deployment/env-prd.sh
+++ deployment/env-prd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-vault kv get -format=json production/prd-pena-web | jq -r '.data.data | to_entries | .[] | "\(.key)=\(.value)"' >> .env
+vault kv get -format=json pena/prd-pena-web | jq -r '.data.data | to_entries | .[] | "\(.key)=\(.value)"' >> .env
